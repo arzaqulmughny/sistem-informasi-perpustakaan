8451f1d2d13e896df3f685028a109c7c29f9fa2a
arza <arza@xenopati.id>
2025-01-05 11:59:14 +0700

    feat-seeder: generate more realistic seeder instead using faker

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,18 +16,21 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $superAdmin = User::factory()->create([
+            'name' => 'Developer',
             'email' => 'developer@demo.com',
             'role_id' => UserRole::SUPER_ADMIN,
             'created_by' => 0,
         ]);
 
         User::factory()->create([
+            'name' => 'Admin',
             'email' => 'adminsip@demo.com',
             'role_id' => UserRole::ADMIN,
             'created_by' => $superAdmin->id
         ]);
 
         User::factory()->create([
+            'name' => 'Staff',
             'email' => 'staff@demo.com',
             'role_id' => UserRole::STAFF,
             'created_by' => $superAdmin->id
